5d28b9145482b57be3c6c6cb83a04b1d84efb1aa
benbc <ben@bridesmere.com>
2010-03-08 23:29:45 +0000

    gimp script

--- scan-clean.scm
+++ scan-clean.scm
@@ -4,4 +4,5 @@
     (gimp-threshold drawable 191 255)
     (plug-in-gauss-iir2 RUN-NONINTERACTIVE image drawable 5.0 5.0)
     (gimp-levels drawable HISTOGRAM-VALUE 111 143 1.0 0 255)
+    (plug-in-colortoalpha RUN-NONINTERACTIVE image drawable '(255 255 255))
     (gimp-file-save RUN-NONINTERACTIVE image drawable file-out file-out)))

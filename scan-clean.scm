42bf34c4c82f1f2387e678942d4cdcb3cc94bb3f
benbc <ben@bridesmere.com>
2010-03-08 23:03:49 +0000

    gimp script

---
+++ scan-clean.scm
@@ -0,0 +1,7 @@
+(define (scan-clean file-in file-out)
+  (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE file-in file-in)))
+         (drawable (car (gimp-image-get-active-layer image))))
+    (gimp-threshold drawable 191 255)
+    (plug-in-gauss-iir2 RUN-NONINTERACTIVE image drawable 5.0 5.0)
+    (gimp-levels drawable HISTOGRAM-VALUE 111 143 1.0 0 255)
+    (gimp-file-save RUN-NONINTERACTIVE image drawable file-out file-out)))
